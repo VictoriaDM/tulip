3f48b99d525a2e2ec294cc6d7ada3bbbdafdfdbf
Victoria Doldán <Sandman@mbp-de-victoria.windows.ebi.ac.uk>
2015-02-13 15:30:53 +0000

    add code to read csv files

--- src/test.py
+++ src/test.py
@@ -1,1 +1,8 @@
 import pandas as pd
+
+# cd /Users/Sandman/Desktop/Drugsfeb2015
+df = pd.read_csv("IC50_v17.csv")
+df2 = pd.read_csv("DRUG_ANALYSIS_SET.csv")
+df3 = pd.read_csv("MASTER_LIST.csv")
+
+
