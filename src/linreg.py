0723ebee9bbd47f2d89956b2157e333a3094be66
cokelaer <cokelaer@gmail.com>
2015-02-18 15:00:12 +0000

    add notebook to show feasibility of estimation beta parameters from FC.

--- src/linreg.py
+++ src/linreg.py
@@ -1,3 +1,13 @@
-# scikit learn for regression
+"""
+Some notebooks/literature on regression
+===========================================
 
-# http://nbviewer.ipython.org/github/temporaer/tutorial_ml_gkbionics/blob/master/3b%20-%20Linear%20regression%202D.ipynb
+* http://nbviewer.ipython.org/github/temporaer/tutorial_ml_gkbionics/blob/master/3b%20-%20Linear%20regression%202D.ipynb
+* http://nbviewer.ipython.org/github/jbpoline/bayfmri/blob/master/notebooks/005-Simple-Linear-Regression.ipynb
+"""
+
+
+
+
+class Regression(object):
+    pass
