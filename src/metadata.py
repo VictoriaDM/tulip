fb2ec9a123d98929e06c14f04a729a338128ef37
cokelaer <cokelaer@gmail.com>
2015-02-20 15:08:58 +0000

    add more information.

--- src/metadata.py
+++ src/metadata.py
@@ -1,3 +1,17 @@
-cell_lines = ['CCK-81', 'COLO-320-HSR', 'DIFI,'HCT-116',
- 'HT-115','HT-29','SK-CO-1',,'SNU-C2B','SNU-C5','W1116',
- 'SW1463',SW620',SW837']
+cell_lines = ['CCK-81', 'COLO-320-HSR', 'DIFI','HCT-116',
+ 'HT-115','HT-29','SK-CO-1','SNU-C2B','SNU-C5','W1116',
+ 'SW1463','SW620','SW837']
+
+
+inhibitors = ['PI3K', 'mTOR', 'MEKi', 'TBK1', 'IKK', 'PLX','TAK']
+
+
+mapping_inhibitor = {
+        1066: 'PI3K', 
+        1059: 'mTOR',
+        1371: 'PLX', #BRAF
+        1498: 'MEK',   # I
+        1037: 'TBK1',
+        203: 'IKK',
+        1242: 'TAK'
+        } 
