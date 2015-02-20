5769a98bb3b8bc2525f2542ac7170738d85c87bb
cokelaer <cokelaer@gmail.com>
2015-02-20 15:34:07 +0000

    add IC50 as json and update cell line identifiers in metadata

--- src/metadata.py
+++ src/metadata.py
@@ -7,11 +7,29 @@ inhibitors = ['PI3K', 'mTOR', 'MEKi', 'TBK1', 'IKK', 'PLX','TAK']
 
 
 mapping_inhibitor = {
-        1066: 'PI3K', 
+        1066: 'PI3K',
         1059: 'mTOR',
         1371: 'PLX', #BRAF
         1498: 'MEK',   # I
         1037: 'TBK1',
         203: 'IKK',
         1242: 'TAK'
-        } 
+        }
+
+mapping_cellLines = {
+    1789883: 'DIFI',
+    1240123: 'CCK81',
+    910569: 'COLO320HSR',
+    1789883:'DIFI',
+    905936: 'HCT116',
+    907289: 'HT115',
+    905939: 'HT29',
+    909718: 'SKCO1',
+    909740: 'SNUC2B',
+    1674021:'SNUC5',
+    909746: 'SW1116',
+    909748: 'SW1463',
+    905962: 'SW620',
+    909755: 'SW837',
+    924108 :'CAR1'
+    } 
