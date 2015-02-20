4aca37183709d7527aa4d0631e10d406f8eb6979
cokelaer <cokelaer@gmail.com>
2015-02-20 16:47:48 +0000

    update notebook with new results.

--- src/linreg.py
+++ src/linreg.py
@@ -7,10 +7,11 @@ Some notebooks/literature on regression
 """
 
 import numpy as np
+import scipy.linalg as la
 
 
 def linear_model(X, beta):
-    """
+    """Returns Y = beta . X
 
 
     :param X: a matrix of shape (N,p)
@@ -27,6 +28,36 @@ def ols(X):
     """
     return np.dot(la.pinv(np.dot(X.T, X)), X.T)
 
+
+
+def estimate_beta(X, Y, noise_level=0):
+    shape = X.shape
+    Xprime = X + np.random.randn(shape[0], shape[1])*noise_level
+    #U,D,V = la.svd(Xprime, full_matrices = False) 
+    #if D.min()<0.001:
+    #    print D
+    beta_hat = np.dot(ols(Xprime), Y)
+    return beta_hat
+
+
+
+
+def ridge_estimate(X, Y, L=10, noise_level=0.1):
+    """" 
+    Calculate the matrix for ridge regression
+    
+    Parameters 
+    ----------
+    X : 2d array
+        The design matrix
+    
+    L : float
+        ridge parameter for regularization    
+    """
+    shape = X.shape
+    Xprime = X + np.random.randn(shape[0], shape[1])*noise_level
+    Xprime = np.dot(la.pinv(np.dot(Xprime.T,X) + L * np.eye(Xprime.shape[-1])), X.T)
+    return np.dot(Xprime, Y)
 
 
 
