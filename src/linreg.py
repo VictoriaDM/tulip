f5ab68b4de83373254d6e41583d1659db7e8ed40
cokelaer <cokelaer@gmail.com>
2015-02-20 15:16:19 +0000

    remove a useless file, add ols and linea_model insode linreg and update notebook.

--- src/linreg.py
+++ src/linreg.py
@@ -6,6 +6,29 @@ Some notebooks/literature on regression
 * http://nbviewer.ipython.org/github/jbpoline/bayfmri/blob/master/notebooks/005-Simple-Linear-Regression.ipynb
 """
 
+import numpy as np
+
+
+def linear_model(X, beta):
+    """
+
+
+    :param X: a matrix of shape (N,p)
+    :param list beta: list of p-length
+
+    """
+    return np.dot(X, beta)
+
+
+def ols(X):
+    """A simple implementation of Ordinary Least Square method
+
+    :param X: matrix which solves the OLS regression problem for full-rank X
+    """
+    return np.dot(la.pinv(np.dot(X.T, X)), X.T)
+
+
+
 
 
 
