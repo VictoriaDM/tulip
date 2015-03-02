97865daaf46eba4de711fe54ceb0afaa8631f69c
cokelaer <cokelaer@gmail.com>
2015-03-02 11:56:46 +0000

    update code

--- src/tulip.py
+++ src/tulip.py
@@ -94,7 +94,13 @@ class Tulip(object):
         return Xtrain, Ytrain, Xtest, Ytest, selection
 
     def get_errors(self, phosphos, alpha=0.1, N=12, iterations=None,
-            progress=True, norm=False):
+            progress=True, norm=False, randomise_X=False):
+        """For a given inhibitors, and for a given set of features (phosphos)
+        return errors for all combo of train/test sets using N=12 cell lines 
+        as training and 2 as test.
+
+
+        """
 
 
         from sklearn.linear_model import ElasticNet, Ridge, LinearRegression, Lasso
@@ -127,7 +133,11 @@ class Tulip(object):
             if random_cells is True:
                 Xtrain, Ytrain, Xtest, Ytest, selection = self.train(phosphos, N=12)
             else:
-                X = self.X[phosphos].copy()
+                if randomise_X:
+                    X = self.get_randomise_X()
+                else:
+                    X = self.X.copy()
+                X = X[phosphos]
                 selection = list(combos[i])
                 indices = [self.cellLines.index(this) for this in combos[i]]
 
@@ -141,7 +151,6 @@ class Tulip(object):
                 Xtest = X.ix[selection_inv]
                 Ytest = self.Y[indices_inv]
 
-
             ridge.fit(Xtrain, Ytrain)
             scores.append(ridge.score(Xtrain, Ytrain))
             percent_error = abs(ridge.predict(Xtest) - Ytest)/abs(Ytest) * 100
@@ -151,8 +160,6 @@ class Tulip(object):
                 selection])
             if progress is True:
                 pb.animate(i, time.time()-pb.start)
-
-        
 
         df = pd.DataFrame({
             'errors':list(flatten(errors)),
@@ -174,7 +181,8 @@ class Tulip(object):
         return all_combos
 
 
-    def get_results(self, alpha=0.01, N=12 , method='ridge', max_combo=14, norm=False):
+    def get_results(self, alpha=0.01, N=12 , method='ridge', max_combo=14,
+            norm=False, randomise_X=False):
 
         self.method = method
         results = {}
@@ -182,13 +190,14 @@ class Tulip(object):
 
         combo_phosphos = combo_phosphos[0:]
         pb = PB(len(combo_phosphos)*len(self.inhibitors))
+
         for j,inhibitor in enumerate(self.inhibitors):
             self.inhibitor = inhibitor
-           
+
             results[inhibitor] = {'i':[], 'combo':[],'errors':[], 'scores':[], 'params':[]}
             for i, combo in enumerate(combo_phosphos):
                 df, scores, params = self.get_errors(list(combo), alpha=alpha, N=N,
-                        progress=False, norm=norm)
+                        progress=False, norm=norm, randomise_X=randomise_X)
                 results[inhibitor]['errors'].append(df['errors'].mean())
                 results[inhibitor]['scores'].append(np.mean(scores))
 
@@ -204,13 +213,53 @@ class Tulip(object):
 
         return scores, errors
 
+    def plot_results(self, scores, errors, save=False, tag=""):
+        import pylab
+        pylab.figure(1)
+        pylab.clf()
+        for inh in scores.columns:
+            pylab.plot(scores[inh], errors[inh], 'o', 
+                    label=inh)
+        pylab.legend(loc='best')
+        pylab.grid()
+        pylab.xlabel("$R^2$ (scores)", fontsize=16)
+        pylab.ylabel("Percentage errors", fontsize=16)
+        pylab.title("%s regression" % self.method.capitalize())
+        if save:
+            pylab.savefig("tulip_reg_%s_%s.png" % (self.method, tag))
+
+    def get_randomise_X(self):
+        data = self.X.copy()[self.X.columns[1:]]
+        for view in np.rollaxis(data.values, 1):
+            np.random.shuffle(view)
+        return data
+
+    def plot_errors_vs_cv_auc(self, errors, yshift=2, xshift=-1):
+        import pylab
+        X = self.IC50.std(axis=1)/self.IC50.mean(axis=1)*100
+        Y=errors.mean()
+        from sklearn.linear_model import LinearRegression
+        model = LinearRegression()
+        model.fit(X.as_matrix().reshape(7,1), Y.as_matrix())
+        print("R2 = ", model.score(X.as_matrix().reshape(7,1), Y))
+        pylab.clf()
+        pylab.plot(X, Y, 'o')
+        pylab.plot(X.values, [x*model.coef_ for x in X.values], 'r')
+        pylab.grid()
+        pylab.xlabel('CV on AUC for each Drug (across cell lines)', fontsize=16)
+        pylab.ylabel('Mean errors (across cell lines)', fontsize=16)
+        for i in range(0,7): 
+            pylab.text(X[i]+xshift, Y[i]+ yshift, X.index[i])
+        pylab.savefig("tulip_errors_function_cv_auc.png")
 
 
 
 
+def get_cv_auc_ic50():
+    """AUC is actually 1-AUC"""
+    t = Tulip()
+    auc = (t.IC50.std()/t.IC50.mean() * 100).mean()
+    t = Tulip(use_auc=False)
+    ic50 = (t.IC50.std()/t.IC50.mean() * 100).mean()
 
-
-
-
-
-
+    return {'cv_ic50': ic50, 'cv_auc':auc}
